a6a5de1298a439c6dd3ddc808a6a68d0ef2ffa8e
Oscar Davies <oscar.william.davies@outlook.com>
2020-05-22 12:18:27 +0100

    Dont Cache anything but UserInfo(CurrencyAccount)

--- Site/Site/Backend/Data/CacheHandler.cs
+++ Site/Site/Backend/Data/CacheHandler.cs
@@ -7,15 +7,11 @@ using Site.Backend.Data.Generics;
 
 namespace Site.Backend.Data
 {
-    public static class CacheHandler
+    public static class DataHandler
     {
         #region Fields
 
-        private static Dictionary<CurrencyConfig, DateTime> _currencyCache = new Dictionary<CurrencyConfig, DateTime>();
-        private static Dictionary<CurrencyParticipant, DateTime> _currencyParticipantCache = new Dictionary<CurrencyParticipant, DateTime>();
-        private static Dictionary<UserAccount, DateTime> _userAccCache = new Dictionary<UserAccount, DateTime>();
         private static Dictionary<_userInfo, DateTime> _userCache = new Dictionary<_userInfo, DateTime>();
-        private static Dictionary<Streamer, DateTime> _streamerCache = new Dictionary<Streamer, DateTime>();
 
         #endregion Fields
 
@@ -42,14 +38,6 @@ namespace Site.Backend.Data
             return temp.ToArray();
         }
 
-        private static UserAccount AddAccount(UserAccount user)
-        {
-            user.Insert();
-            user = UserAccount.Find(user.user);
-            _userAccCache.Add(user, DateTime.Now);
-            return user;
-        }
-
         private static _userInfo AddUser(_userInfo user)
         {
             user.Insert();
@@ -58,61 +46,33 @@ namespace Site.Backend.Data
             return user;
         }
 
-        private static Streamer AddStreamer(Streamer strmr)
-        {
-            strmr.Insert();
-            strmr = Streamer.Find(strmr.twitch_name);
-            _streamerCache.Add(strmr, DateTime.Now);
-            return strmr;
-        }
-
         public static Streamer FindStreamer(uint streamerid)
         {
-            Streamer _streamer = Find(_streamerCache, x => x.Id == streamerid);
-
-            if (_streamer == null)
-            {
-                _streamer = Streamer.Find(streamerid);
-                if (_streamer != null) _streamerCache.Add(_streamer, DateTime.Now);
-            }
-
-            return _streamer;
+            
+               return Streamer.Find(streamerid);
         }
 
         public static Streamer FindStreamer(string twitch_name, string twitch_email)
         {
-            Streamer _streamer = Find(_streamerCache, x => x.twitch_name == twitch_name);
-
-            if (_streamer == null)
-            {
-                _streamer = Streamer.Find(twitch_name);
-                if (_streamer != null) _streamerCache.Add(_streamer, DateTime.Now);
-            }
+            Streamer _streamer = Streamer.Find(twitch_name);
 
             if (_streamer == null)
             {
                 _streamer = new Streamer(twitch_name, twitch_email);
-                return AddStreamer(_streamer);
+                _streamer.Insert();
             }
-
 
             return _streamer;
         }
 
         public static UserAccount FindAccount(User User)
         {
-            UserAccount _userAcc = Find(_userAccCache, x => x.user.Equals(User));
-
-            if (_userAcc == null)
-            {
-                _userAcc = UserAccount.Find(User);
-                if (_userAcc != null) _userAccCache.Add(_userAcc, DateTime.Now);
-            }
+            UserAccount _userAcc = UserAccount.Find(User);
 
             if (_userAcc == null)
             {
                 _userAcc = new UserAccount(User);
-                return AddAccount(_userAcc);
+                _userAcc.Insert();
             }
 
             return _userAcc;
@@ -120,28 +80,13 @@ namespace Site.Backend.Data
 
         public static UserAccount FindAccount(uint AccId)
         {
-            UserAccount _userAcc = Find(_userAccCache, x => x.Id == AccId);
-
-            if (_userAcc == null)
-            {
-                _userAcc = UserAccount.Find(AccId);
-                if (_userAcc != null) _userAccCache.Add(_userAcc, DateTime.Now);
-            }
-
-            return _userAcc;
+            return UserAccount.Find(AccId);
         }
 
         public static CurrencyConfig FindCurrency(uint currencyId)
         {
-            CurrencyConfig _currency = Find(_currencyCache, x => x.Id == currencyId);
 
-            if (_currency == null)
-            {
-                _currency = CurrencyConfig.Find(currencyId);
-                if (_currency != null) _currencyCache.Add(_currency, DateTime.Now);
-            }
-
-            return _currency;
+            return CurrencyConfig.Find(currencyId);
         }
 
         public static CurrencyConfig[] FindCurrencyByStreamer(uint streamerid)
@@ -151,15 +96,10 @@ namespace Site.Backend.Data
 
         public static CurrencyConfig FindCurrency(string sIdentifier, Source source)
         {
-            CurrencyParticipant _participant = Find(_currencyParticipantCache, x => (x.discord_guild == sIdentifier && source == Generics.Source.Discord) || (x.twitch_name == sIdentifier && source == Generics.Source.Twitch));
+            CurrencyParticipant _participant;
 
-            if (_participant == null)
-            {
-                if (source == Generics.Source.Discord) _participant = CurrencyParticipant.FindDiscord(sIdentifier);
-                else _participant = CurrencyParticipant.FindTwitch(sIdentifier);
-
-                if (_participant != null) _currencyParticipantCache.Add(_participant, DateTime.Now);
-            }
+            if (source == Generics.Source.Discord) _participant = CurrencyParticipant.FindDiscord(sIdentifier);
+            else _participant = CurrencyParticipant.FindTwitch(sIdentifier);
 
             return FindCurrency(_participant.currencyid);
         }
