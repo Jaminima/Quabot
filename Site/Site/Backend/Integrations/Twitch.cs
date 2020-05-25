1bf0e54018251e5585a67d2f3c02cc828b03d36f
Oscar Davies <oscar.william.davies@outlook.com>
2020-05-25 15:52:14 +0100

    Demo of Loading profile data

---
+++ Site/Site/Backend/Integrations/Twitch.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using Site.Backend.Data;
+
+namespace Site.Backend.Integrations
+{
+    public static class Twitch
+    {
+        public static async Task<JToken> GetUser(string AuthToken)
+        {
+            return await Web.DoReq("https://api.twitch.tv/helix/users",
+                Headers:new string[] { "Authorization", "Bearer " + AuthToken, "Client-ID",Config.Conf.twitch_sign_client_id });
+        }
+    }
+}
---
+++ Site/Site/Backend/Integrations/Web.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using System.IO;
+
+namespace Site.Backend.Integrations
+{
+    public static class Web
+    {
+        public static async Task<JToken> DoReq(string URL, string Method="GET", string[] Headers=null)
+        {
+            WebRequest Req = WebRequest.Create(URL);
+            Req.Method = Method;
+
+            for (uint i = 0; i < Headers?.Length; i += 2) Req.Headers.Add(Headers[i], Headers[i + 1]);
+
+            try
+            {
+                WebResponse Res = await Req.GetResponseAsync();
+                StreamReader stream = new StreamReader(Res.GetResponseStream());
+                string Cont = stream.ReadToEnd();
+                return JToken.Parse(Cont);
+            }
+            catch { return null; }
+        }
+    }
+}
