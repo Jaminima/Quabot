9042ec2870c95d84c72c2ca8773183a4c48ee89a
Oscar Davies <oscar.william.davies@outlook.com>
2020-05-26 18:05:30 +0100

    Add Auth Redirects

--- Site/Site/Backend/Authorization/AuthAction.cs
+++ Site/Site/Backend/Authorization/AuthAction.cs
@@ -16,19 +16,43 @@ namespace Site.Backend.Authorization
 {
     public static class AuthAction
     {
-        public static void GoDiscordBot(NavigationManager nav)
+        public static void GoDiscordBot(NavigationManager nav, string redirectURL="")
         {
-            GoOauthTo("https://discordapp.com/oauth2/authorize?client_id=" + Config.Conf.discord_bot_client_id + "&scope=bot&permissions=0&response_type=code&redirect_uri=" + new UriBuilder(Config.Conf.oAuthRedirect).ToString() + "&state=identifier%3D{0}%26auth_mode=discord-bot", nav);
+            GoOauthTo("https://discord.com/api/oauth2/authorize", "bot", "discord-bot", redirectURL, 0, nav);
+            //GoOauthTo("https://discordapp.com/oauth2/authorize?client_id=" + Config.Conf.discord_bot_client_id + "&scope=bot&permissions=0&response_type=code&redirect_uri=" + new UriBuilder(Config.Conf.oAuthRedirect).ToString() + "&state=identifier%3D{0}%26auth_mode=discord-bot", nav);
         }
 
-        public static void GoDiscordSignin(NavigationManager nav)
+        public static void GoDiscordSignin(NavigationManager nav, string redirectURL = "")
         {
-            GoOauthTo("https://discord.com/api/oauth2/authorize?client_id=" + Config.Conf.discord_bot_client_id + "&redirect_uri=" + new UriBuilder(Config.Conf.oAuthRedirect).ToString() + "&response_type=code&scope=email+identify&state=identifier%3D{0}%26auth_mode=discord-login", nav);
+            GoOauthTo("https://discord.com/api/oauth2/authorize", "email+identify", "discord-login", redirectURL, 0, nav);
+            //GoOauthTo("https://discord.com/api/oauth2/authorize?client_id=" + Config.Conf.discord_bot_client_id + "&redirect_uri=" + new UriBuilder(Config.Conf.oAuthRedirect).ToString() + "&response_type=code&scope=email+identify&state=identifier%3D{0}%26auth_mode=discord-login", nav);
         }
 
-        public static void GoTwitchSignin(NavigationManager nav)
+        public static void GoTwitchSignin(NavigationManager nav, string redirectURL = "")
         {
-            GoOauthTo("https://id.twitch.tv/oauth2/authorize?response_type=code&client_id=" + Config.Conf.twitch_sign_client_id + "&redirect_uri=" + Config.Conf.oAuthRedirect + "&scope=user_read+user:read:email&state=identifier%3D{0}%26auth_mode=twitch-login",nav);
+            GoOauthTo("https://id.twitch.tv/oauth2/authorize", "user_read+user:read:email", "twitch-login", redirectURL, 1, nav);
+            //GoOauthTo("https://id.twitch.tv/oauth2/authorize?response_type=code&client_id=" + Config.Conf.twitch_sign_client_id + "&redirect_uri=" + Config.Conf.oAuthRedirect + "&scope=user_read+user:read:email&state=identifier%3D{0}%26auth_mode=twitch-login",nav);
+        }
+
+        private static void GoOauthTo(string URL, string scope, string authMode, string redirectURL, int Src, NavigationManager nav)
+        {
+            URL += "?";
+            URL += "response_type=code";
+            if (Src == 0)//Discord Bot
+            {
+                URL += "&redirect_uri=" + new UriBuilder(Config.Conf.oAuthRedirect).ToString();
+                URL += "&client_id=" + Config.Conf.discord_bot_client_id;
+            }
+            else if (Src == 1)//Twitch
+            {
+                URL += "&redirect_uri=" + Config.Conf.oAuthRedirect;
+                URL += "&client_id=" + Config.Conf.twitch_sign_client_id;
+            }
+
+            URL += "&scope=" + scope;
+            URL += "&state=identifier%3D{0}%26auth_mode="+authMode+ "%26redirect="+redirectURL;
+
+            GoOauthTo(URL, nav);
         }
 
         private static void GoOauthTo(string URL, NavigationManager nav)
